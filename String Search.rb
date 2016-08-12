9bb45274619428589b04911f18582568b4fae8bc
Parnab Sanyal <sanyal.parnab96@gmail.com>
2016-08-13 01:54:21 +0530

    Search Pattern in a string

---
+++ String Search.rb
@@ -0,0 +1,19 @@
+File.open(ARGV[0]).each_line do |line|
+	expr,regex = gets.chomp.split(",")
+	i = 0
+	while i < regex.length do 
+		if regex[i] == "*" && regex[i - 1] != "\\"
+			part1 = regex[0...i]
+			part2 = "\\w"
+			part3 = regex[i..-1]
+			regex = part1 << part2 << part3
+			i += 2
+		end
+		i += 1
+	end
+	if expr.match(regex)
+		puts true
+	else
+		puts false
+	end
+end
